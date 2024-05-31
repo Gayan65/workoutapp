04486419280a20c85c0db35214910d275e50aa7b
Gayan65 <gayan65728@yahoo.com>
2024-05-31 22:51:39 +0300

    api endpoints add

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,8 +2,29 @@ import express from "express";
 
 const router = express.Router();
 
+//GET All workouts
 router.get("/", (req, res) => {
-    res.json({ msg: "running successfully  !" });
+    res.json({ msg: "GET All workouts" });
+});
+
+//GET a single workout
+router.get("/:id", (req, res) => {
+    res.json({ msg: "GET a single workout" });
+});
+
+//POST a workout
+router.post("/", (req, res) => {
+    res.json({ msg: "POST a workout" });
+});
+
+//DELETE a workout
+router.delete("/:id", (req, res) => {
+    res.json({ msg: "DELETE a workout" });
+});
+
+//UPDATE a workout
+router.patch("/:id", (req, res) => {
+    res.json({ msg: "UPDATE a workout" });
 });
 
 export default router;
