be05a7eb93be1a23251cf0c9cb8bccf521a95a0f
Gayan65 <gayan65728@yahoo.com>
2024-06-01 10:56:21 +0300

    workout post req add

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -14,8 +14,15 @@ router.get("/:id", (req, res) => {
 });
 
 //POST a workout
-router.post("/", (req, res) => {
-    res.json({ msg: "POST a workout" });
+router.post("/", async (req, res) => {
+    const { title, reps, load } = req.body;
+
+    try {
+        const workout = await Workout.create({ title, reps, load });
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 //DELETE a workout
