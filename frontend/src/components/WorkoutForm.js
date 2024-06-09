d2fbca935ab49de428c2e203ef7f8aea62e92d67
Gayan65 <gayan65728@yahoo.com>
2024-06-09 17:48:04 +0300

    form component

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState("");
+    const [load, setLoad] = useState("");
+    const [reps, setReps] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form className="create" onSubmit={handleSubmit}>
+            <h1>Create Workout</h1>
+            <label>Exercise Tile:</label>
+            <input
+                type="text"
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+            <label>Load (in kg):</label>
+            <input
+                type="number"
+                onChange={(e) => setLoad(e.target.value)}
+                value={load}
+            />
+            <label>Reps:</label>
+            <input
+                type="number"
+                onChange={(e) => setReps(e.target.value)}
+                value={reps}
+            />
+            <button>Submit</button>
+        </form>
+    );
+};
+
+export default WorkoutForm;
