9ca4c2c861ca8ceefbf7796013311faa990499c1
Gayan65 <gayan65728@yahoo.com>
2024-06-15 12:03:40 +0300

    token JWT add

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import { User } from "../models/UserModel.js";
+
+//create jwt
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //user login function
 export const userLogin = async (req, res) => {
@@ -12,7 +18,11 @@ export const userSignup = async (req, res) => {
 
     try {
         const user = await User.signup(email, password);
-        res.status(200).json({ email, user });
+
+        //passing the token
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
